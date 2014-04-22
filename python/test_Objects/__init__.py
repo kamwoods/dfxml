fb48056b0a8830557ad6bd4a8e107cd9b2ce80f1
BitCurator <bcadmin@ubuntu.(none)>
2014-04-21 20:09:52 -0400

    Updated test init

---
+++ python/test_Objects/__init__.py
@@ -0,0 +1,9 @@
+__all__ = [
+'test_ByteRun.py', 'test_ByteRuns.py',
+'test_diffing_ByteRuns.py', 'test_diffing_CellObject.py',
+'test_diffing_FileObject.py', 'test_diffing_HiveObject.py',
+'test_diffing_TimestampObject.py', 'test_diffing_VolumeObject.py',
+'test_FileObject_byte_run_facets.py', 'test_FileObject_from_stat.py',
+'test_RegXMLOBject.py', 'test_VolumeObject_hash.py',
+'verify_differential_dfxml_01.py', 'verify_differential_dfxml_23.py'
+]
